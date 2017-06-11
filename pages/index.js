a81e598997f276b230d27ef361c9f369f98b0251
Tim Man <timothyc.man@gmail.com>
2017-06-12 01:16:24 +0800

    move css reset into own component

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export const Index = () => (
     <main>
       <Grid items={gridItems()}/>
     </main>
+    <CSSReset />
     <style jsx>{`
       nav {
         display: flex;
@@ -32,44 +33,51 @@ export const Index = () => (
         margin-top: 7em;
         padding: 0.1em;
         z-index: 2;
+        overflow-x: hidden;
       }
     `}</style>
     <style jsx global>{`
-      html, body, div, span, applet, object, iframe,
-      h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-      a, abbr, acronym, address, big, cite, code,
-      del, dfn, em, font, img, ins, kbd, q, s, samp,
-      small, strike, strong, sub, sup, tt, var,
-      dl, dt, dd, ol, ul, li,
-      fieldset, form, label, legend,
-      table, caption, tbody, tfoot, thead, tr, th, td {
-        margin: 0;
-        padding: 0;
-        border: 0;
-        outline: 0;
-        font-weight: inherit;
-        font-style: inherit;
-        font-size: 100%;
-        font-family: inherit;
-        vertical-align: baseline;
-      }
-      html {
-        box-sizing: border-box;
-      }
-      *, *:before, *:after {
-        box-sizing: inherit;
-      }
       body {
         font-family: sans-serif;
         width: 100%;
-      }
-      ol, ul {
-        list-style: none;
+        overflow-x: hidden;
       }
     `}</style>
   </div>
 )
 export default Index
+
+const CSSReset = () => (
+  <style jsx global>{`
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, font, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      outline: 0;
+      font-weight: inherit;
+      font-style: inherit;
+      font-size: 100%;
+      font-family: inherit;
+      vertical-align: baseline;
+    }
+    html {
+      box-sizing: border-box;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+    ol, ul {
+      list-style: none;
+    }
+  `}</style>
+);
 
 // TODO move to redux state and props?
 const filterItems = () => ([
