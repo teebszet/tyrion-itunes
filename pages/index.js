73a0ff5fd5a25ec5cf29b160e65d536f557f685c
Tim Man <timothyc.man@gmail.com>
2018-10-10 16:56:24 +0800

    put isomorphic-unfetch back in

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-// import fetch from 'isomorphic-unfetch'
+import fetch from 'isomorphic-unfetch'
 import _pick from 'lodash/pick'
 
 import CSSReset from '../components/CSSReset'
@@ -19,33 +19,34 @@ export async function getLayoutInitialProps({
   const {search = 'the'} = query; // default query to 'the'
   // TODO use a uri builder
   const uri = `https://itunes.apple.com/search?media=${media}&term=${search}&limit=12`;
+  let data = {};
   try {
     const res = await fetch(uri);
     if (!res.ok) {
       console.error(res);
       throw "response not ok";
     }
-    const data = await res.json();
+    data = await res.json();
     console.log('result count: ', data.resultCount);
     console.log(uri);
-    return {
-      ...data,
-      results: data.results ? data.results
-        .map(item => ({
-          ..._pick(item, [
-            'artworkUrl600',
-            'trackName',
-            'longDescription',
-            'trackViewUrl',
-            'trackId',
-          ]), 
-          image: item.artworkUrl600 || item.artworkUrl100
-        })) : [],
-      selectedFilter: media,
-    }
   } catch(e) {
     console.log(e);
     return {};
+  }
+  return {
+    ...data,
+    results: data.results ? data.results
+      .map(item => ({
+        ..._pick(item, [
+          'artworkUrl600',
+          'trackName',
+          'longDescription',
+          'trackViewUrl',
+          'trackId',
+        ]),
+        image: item.artworkUrl600 || item.artworkUrl100
+      })) : [],
+    selectedFilter: media,
   }
 }
 Index.getInitialProps = getLayoutInitialProps;
